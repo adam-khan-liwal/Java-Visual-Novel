adc63f790224ca49e2349456053045ffe672c7ad
jegan019 <jp922@uowmail.edu.au>
2017-03-31 22:28:04 +0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -18,8 +18,6 @@ public class Game extends StateBasedGame{
    }
    
    public void initStatesList(GameContainer gc) throws SlickException{
-	  this.getState(menu).init(gc, this);
-      this.getState(play).init(gc, this);
       this.enterState(menu);
    }
    
